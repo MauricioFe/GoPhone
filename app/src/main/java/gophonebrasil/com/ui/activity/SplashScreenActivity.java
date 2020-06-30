23642232edc86a8f44849006adf5030a9aadf38c
MauricioFe <mauricio.lacerdaml@gmail.com>
2020-06-30 15:58:01 -0300

    Implementação da SplashScreenActivity

--- app/src/main/java/gophonebrasil/com/ui/activity/SplashScreenActivity.java
+++ app/src/main/java/gophonebrasil/com/ui/activity/SplashScreenActivity.java
@@ -2,7 +2,9 @@ package gophonebrasil.com.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import gophonebrasil.com.R;
 
@@ -12,5 +14,23 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        mostraSplash();
+    }
+
+    private void mostraSplash() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mostraLogin();
+            }
+
+        },2000);
+    }
+
+    private void mostraLogin() {
+        Intent intent = new Intent();
+        startActivity(intent);
+        finish();
     }
 }
